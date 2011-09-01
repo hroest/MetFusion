555c5b62d0400c9a39a1d18b7eda903456a4d0b3
Michael Gerlich <mgerlich@ipb-halle.de>
2011-09-01 14:57:59 +0000

    Added draft for MetFusionThread in order to separate backing bean and thread execution.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@2699 88837528-0963-4635-b47b-91f4607bead6

---
+++ src/main/java/de/ipbhalle/metfusion/threading/MetFusionThread.java
@@ -0,0 +1,31 @@
+/**
+ * created by Michael Gerlich, Sep 1, 2011 - 4:15:14 PM
+ */ 
+
+package de.ipbhalle.metfusion.threading;
+
+public class MetFusionThread implements Runnable {
+
+	public MetFusionThread() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public synchronized void guardedRetrieval() {
+		
+	}
+	
+	public synchronized void guardedMatrix() {
+		
+	}
+	
+	public synchronized void guardedImageGeneration() {
+		
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
