eb8394bafa91ce27a96ea62cc1563f774ca2b088
Michael Gerlich <mgerlich@ipb-halle.de>
2011-09-15 13:08:50 +0000

    Added business logic for run() method. Now correctly calls threads and finishes. Works with ICEfaces as this thread can now be properly suspended (waiting) while progress bars keep running in web page.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@2756 88837528-0963-4635-b47b-91f4607bead6

--- src/main/java/de/ipbhalle/metfusion/threading/MetFusionThread.java
+++ src/main/java/de/ipbhalle/metfusion/threading/MetFusionThread.java
@@ -4,10 +4,38 @@
 
 package de.ipbhalle.metfusion.threading;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import de.ipbhalle.MassBank.MassBankLookupBean;
+import de.ipbhalle.metfusion.integration.Similarity.SimilarityMetFusion;
+import de.ipbhalle.metfusion.integration.Tanimoto.TanimotoIntegrationWeighted;
+import de.ipbhalle.metfusion.integration.Tanimoto.TanimotoSimilarity;
+import de.ipbhalle.metfusion.web.controller.MetFragBean;
+import de.ipbhalle.metfusion.web.controller.MetFusionBean;
+import de.ipbhalle.metfusion.web.controller.ResultExtGroupBean;
+import de.ipbhalle.metfusion.web.controller.StyleBean;
+import de.ipbhalle.metfusion.wrapper.Result;
+import de.ipbhalle.metfusion.wrapper.ResultExt;
+
 public class MetFusionThread implements Runnable {
 
-	public MetFusionThread() {
-		// TODO Auto-generated constructor stub
+	private MetFusionBean metfusion;
+	private MassBankLookupBean massbank;
+	private MetFragBean metfrag;
+	private StyleBean styleBean;
+	private String tempPath;
+	private int progress;
+	private boolean active = Boolean.FALSE;
+	private int steps = 0;
+	private final int totalSteps = 8;
+	
+	public MetFusionThread(MetFusionBean app, MassBankLookupBean database, MetFragBean fragmenter, StyleBean styleBean, String tempPath) {
+		this.metfusion = app;
+		this.massbank = database;
+		this.metfrag = fragmenter;
+		this.styleBean = styleBean;
+		this.tempPath = tempPath;
 	}
 	
 	/**
@@ -33,8 +61,139 @@ public class MetFusionThread implements Runnable {
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		long time1 = System.currentTimeMillis();
+		setActive(Boolean.TRUE);
+		setProgress(steps);
+		metfusion.setStatus("Retrieval");
 		
+		massbank.run();
+		metfrag.run();
+		while(!massbank.isDone() && !metfrag.isDone()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		steps += 2;
+		stepsDonePercent(steps);
+		metfusion.setShowResultsDatabase(true);
+		System.out.println("done threading");
+		metfusion.setStatus("Images + Matrix");
+		// create tanimoto matrix and perform chemical-similarity based integration
+		List<Result> listMassBank = massbank.getResults();
+		List<Result> listMetFrag = metfrag.getResults();
+		TanimotoSimilarity sim = new TanimotoSimilarity(listMassBank, listMetFrag);	//, 3, 0.5f);
+		String sessionPath = massbank.getSessionPath();
+		// fork new thread for generating ColorCodedMatrix
+		ColoredMatrixGeneratorThread cmT = new ColoredMatrixGeneratorThread(sim);
+		TanimotoIntegrationWeighted tiw = new TanimotoIntegrationWeighted(sim);
+		// fork new thread for generating compound images
+		ImageGeneratorThread igT = new ImageGeneratorThread(listMetFrag, sessionPath, tempPath);
+		ImageGeneratorThread igT2 = new ImageGeneratorThread(listMassBank, sessionPath, tempPath);
+		
+//		ExecutorService threadExecutor = Executors.newFixedThreadPool(4);
+//        threadExecutor.execute(tiw);
+//        threadExecutor.execute(cmT);
+//        threadExecutor.execute(igT);
+//        threadExecutor.execute(igT2);
+//        threadExecutor.shutdown();
+        
+        tiw.run();
+        cmT.run();
+        igT.run();
+        igT2.run();
+        
+        /**
+		 * MetFrag cluster ranks
+		 */
+        while(!tiw.isDone() && !cmT.isDone() && !igT.isDone() && !igT2.isDone()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+        steps += 4;
+        stepsDonePercent(steps);
+        metfusion.setColorMatrix(cmT.getCcm());
+        
+        List<ResultExt> resultingOrder = tiw.getResultingOrder();
+		List<Result> redraw = new ArrayList<Result>();
+		for (int i = 0; i < resultingOrder.size(); i++) {
+			ResultExt r = resultingOrder.get(i);
+			redraw.add(new Result(r));
+		}
+		
+		/**
+		 *  new colored similarity matrix after metfusion
+		 */
+		TanimotoSimilarity after = new TanimotoSimilarity(listMassBank, redraw);	//, 3, 0.5f);
+		// fork new thread for generating ColorCodedMatrix
+		ColoredMatrixGeneratorThread cmtAfter = new ColoredMatrixGeneratorThread(after);
+		cmtAfter.run();
+		while(!cmtAfter.isDone()) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		steps++;
+		stepsDonePercent(steps);
+        metfusion.setColorMatrixAfter(cmtAfter.getCcm());
+        metfusion.setStatus("Clustering");
+        
+        metfusion.setSecondOrder(resultingOrder);	// assign results to metfusion bean
+		SimilarityMetFusion sm = new SimilarityMetFusion();
+		System.out.println("Started clustering");
+		List<ResultExtGroupBean> clusters = sm.computeScoresCluster(resultingOrder, styleBean);
+		metfusion.setTanimotoClusters(clusters);
+		System.out.println("Finished clustering");
+		metfusion.generateOutputResource();
+		metfusion.setShowTable(true);
+		metfusion.setSelectedTab("1");
+		metfusion.setShowResultTable(true);
+		metfusion.setShowClusterResults(true);
+		
+		steps++;
+		stepsDonePercent(steps);
+		//setProgress(100);
+		System.out.println("list size -> " + clusters.size());
+		setActive(Boolean.FALSE);
+		metfusion.setEnableStart(Boolean.TRUE);
+		long time2 = System.currentTimeMillis() - time1;
+		System.out.println("time spended -> " + time2 + " ms");
+	}
+
+	/**
+	 * Compute the percentage progress depending on the steps accomplished.
+	 * If steps is larger than totalSteps, it is set to totalSteps, thus leading
+	 * to 100%.
+	 * 
+	 * @param steps - the number of steps already taken
+	 */
+	public void stepsDonePercent(int steps) {
+		if(steps > totalSteps)	// ensure upper bound
+			steps = totalSteps;
+		float temp = ((float) steps  * 100f) / (float) totalSteps;
+		setProgress(Math.round(temp));
+	}
+	
+	public void setProgress(int progress) {
+		this.progress = progress;
+	}
+
+	public int getProgress() {
+		return progress;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public boolean isActive() {
+		return active;
 	}
 
 }
