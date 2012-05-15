8174c3372a46b50faa388f7b216dca4692dc7d4f
Michael Gerlich <mgerlich@ipb-halle.de>
2012-05-15 08:44:54 +0000

    Updated and corrected adducts list.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@3548 88837528-0963-4635-b47b-91f4607bead6

--- src/main/java/de/ipbhalle/enumerations/Adducts.java
+++ src/main/java/de/ipbhalle/enumerations/Adducts.java
@@ -8,16 +8,27 @@ package de.ipbhalle.enumerations;
 public enum Adducts {
 	
 	Neutral(0d, "Neutral"),
+	
 	Mplus(0.00054858d, "M+"),
 	MplusHplus(-1.007276455d, "[M+H+]"),
 	MplusNaplus(-22.98921912d, "[M+Na]+"),
 	MplusKplus(-38.96315882d, "[M+K]+"),
 	MplusNH4plus(-18.0385d, "[M+NH4]+"),
-	MplusHCOOminus(-45.0174, "[M+HCOO]-"),
+	MplusMethanolplus(-33.033489d, "[M+CH3OH+H]+"),
+	MplusClplus(-34.969402d, "[M+Cl]+"),
+	MplusFAplus(-44.998201d, "[M+FA-H]+"),
+	Mplus2Naplus(-44.971160d, "[M+2Na-H]+"),
+	MplusIsoPropplus(-61.06534d, "[M+IsoProp+H]+"),
+	MplusACNplus(-42.033823d, "[M+ACN+H]+"),
+	
 	Mminus(-0.00054858d, "M-"),
 	MminusHminus(1.007276455d, "[M-H]-"),
-	MminusNaminus(22.98921912d, "[M-Na]-"),
-	MminusKminus(38.96315882d, "[M-K]-");
+	MplusClminus(-34.969402d, "[M+Cl]-"),
+	MplusNaminus(-20.974666d, "[M+Na-2H]-"),
+	MplusKminus(-36.948606d, "[M+K-2H]-"),
+	MplusFAminus(-44.998201d, "[M+FA-H]-"),
+	MminusWater(19.01839d, "[M-H2O-H]-"),
+	MplusHCOOminus(-45.0174, "[M+HCOO]-"),;
 	
 	private double difference;
 	private String label;
