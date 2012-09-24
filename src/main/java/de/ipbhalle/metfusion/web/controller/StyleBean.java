7b99aacdb1044ff77bfbdd4fe4ad8145b8827792
Michael Gerlich <mgerlich@ipb-halle.de>
2012-09-24 08:27:06 +0000

    Updated serial ID and contributor statement.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@4059 88837528-0963-4635-b47b-91f4607bead6

--- src/main/java/de/ipbhalle/metfusion/web/controller/StyleBean.java
+++ src/main/java/de/ipbhalle/metfusion/web/controller/StyleBean.java
@@ -1,23 +1,3 @@
-/*
-*
-* Copyright (C) 2009-2010 IPB Halle, Sebastian Wolf
-*
-* Contact: swolf@ipb-halle.de
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*/
 package de.ipbhalle.metfusion.web.controller;
 
 /*
@@ -38,7 +18,7 @@ package de.ipbhalle.metfusion.web.controller;
  * Technologies Canada, Corp. Portions created by ICEsoft are Copyright (C)
  * 2004-2006 ICEsoft Technologies Canada, Corp. All Rights Reserved.
  *
- * Contributor(s): _____________________.
+ * Contributor(s): Michael Gerlich.
  *
  * Alternatively, the contents of this file may be used under the terms of
  * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"
@@ -190,11 +170,15 @@ public class StyleBean implements Serializable {
     }
 
     /**
-     * Utility class to manage different cssPath and imageDir namd
+     * Utility class to manage different cssPath and imageDir named
      */
     public class StylePath implements Serializable{
 
-        private String cssPath;
+        /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private String cssPath;
         private String imageDirPath;
 
         public StylePath(String cssPath, String imageDirPath) {
