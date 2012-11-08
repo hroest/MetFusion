09eea67cae9403a38f41747a4fddab66d361df99
Michael Gerlich <mgerlich@ipb-halle.de>
2012-11-08 14:56:39 +0000

    Updated settings.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@4175 88837528-0963-4635-b47b-91f4607bead6

--- src/test/java/de/ipbhalle/GridEngine/main/GenerateArrayJob.java
+++ src/test/java/de/ipbhalle/GridEngine/main/GenerateArrayJob.java
@@ -9,6 +9,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 
+import de.ipbhalle.io.FileNameFilterImpl;
+
 public class GenerateArrayJob {
 
 	/**
@@ -16,17 +18,25 @@ public class GenerateArrayJob {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		File spectraDir = new File("/home/mgerlich/Datasets/allSpectra/");
+		//File spectraDir = new File("/home/mgerlich/Datasets/allSpectra/");
+		//File spectraDir = new File("/home/mgerlich/Datasets/Eawag_IPB_TP_all/recdata/");
 		//File spectraDir = new File("/home/mgerlich/Datasets/Uni_Jena/recdata/");
 		//File spectraDir = new File("/home/mgerlich/Datasets/Pesticides_new/Pesticides_111028/recdata/");
-		File[] files = spectraDir.listFiles();
+		//File spectraDir = new File("/vol/data_extern/ryona@psc.riken.jp/queries20121003/lipid_MS2/120918_007P_pos_ammonium_adduct/");
+		//File spectraDir = new File("/home/mgerlich/projects/jan_stanstrup/rungroup6/");
+		File spectraDir = new File("/vol/data_extern/ryona@psc.riken.jp/queries20121003/secmet_QTOF/root_n_secmet_QTOF/");
+		File[] files = spectraDir.listFiles(new FileNameFilterImpl("", "txt"));
 		Arrays.sort(files);
 		
 		// the output directory of the grid engine runs
 		//String outputDir = "/home/mgerlich/projects/metfusion_CE_spectra/";
 		//String outputDir = "/home/mgerlich/projects/eval_metfusion_ECFP/exactMass_noFilter/";
 		//String outputDir = "/home/mgerlich/projects/metfusion_pesticide_spectra/";
-		String outputDir = "/home/mgerlich/projects/eval_metfusion_uniqueFilter_14092012/";
+		//String outputDir = "/home/mgerlich/projects/eval_metfusion_uniqueFilter_14092012/";
+		//String outputDir = "/home/mgerlich/projects/metfusion_tp/results_18-10-2012";
+		//String outputDir = "/vol/data_extern/ryona@psc.riken.jp/queries20121003/results/120918_007P_pos_ammonium_adduct/";
+		//String outputDir = "/home/mgerlich/projects/jan_stanstrup/results/rungroup6/";
+		String outputDir = "/vol/data_extern/ryona@psc.riken.jp/queries20121003/secmet_QTOF/root_n_secmet_QTOF/results/";
 
 		// the directory in which the jar file is located
 		String projectDir = "/home/mgerlich/projects/";
@@ -34,7 +44,7 @@ public class GenerateArrayJob {
 		// the directory where the shell scripts are stored
 		//String workDir = "/home/mgerlich/projects/eval_metfusion_ECFP/";
 		String workDir = projectDir;
-		String prefix = "uniqueFilter";
+		String prefix = "root_n";
 		File jobInfo = new File(workDir, prefix + "_sge_metfusion.sh");
 		jobInfo.createNewFile();
 		jobInfo.setExecutable(true);
@@ -50,7 +60,7 @@ public class GenerateArrayJob {
 		File scriptName = new File(workDir, prefix + "_sge_metfusion.q");
 		generateShellScript(scriptName, paramFile.getAbsolutePath(), jarName.getAbsolutePath());
 		
-		generateQSUB(jobInfo, scriptName.getAbsolutePath(), "MSBIoffice", 1, files.length);
+		generateQSUB(jobInfo, scriptName.getAbsolutePath(), "MSBI", 1, files.length);
 	}
 
 	public static void generateShellScript(File f, String paramFile, String jarName) throws IOException {
@@ -59,7 +69,9 @@ public class GenerateArrayJob {
 		
 		// adjust according to number of parameters
 		fw.write("awk \"NR==$SGE_TASK_ID\" " + paramFile + " | while read A B; do echo $A/$B\n");
-		fw.write("java -jar -Dproperty.file.path=/home/mgerlich/workspace_new/MetFusion/WebContent/WEB-INF/ " + jarName + " -record $A -out $B -unique\n");
+		//fw.write("java -jar -Dproperty.file.path=/home/mgerlich/workspace_new/MetFusion/WebContent/WEB-INF/ " + jarName + " -mf $A -out $B -unique\n");
+		fw.write("java -jar -Xmx4000m -Dproperty.file.path=/home/mgerlich/workspace_new/MetFusion/WebContent/WEB-INF/ " + jarName 
+				+ " -mf $A -out $B -unique\n");
 		//fw.write("EOF\n");
 		fw.write("done");
 		
@@ -83,7 +95,8 @@ public class GenerateArrayJob {
 		FileWriter fw = new FileWriter(f);
 		fw.write("#!/bin/bash\n\n");	// shell header
 		
-		fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/ -o /home/mgerlich/SGE/output/ -q " + queue + " " + qFile + "\n");
+		//fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/ryona/ -o /home/mgerlich/SGE/output/ryona/ -q " + queue + " " + qFile + "\n");
+		fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/secmet/ -o /home/mgerlich/SGE/output/secmet/ -q " + queue + " " + qFile + "\n");
 		
 		fw.flush();
 		fw.close();
