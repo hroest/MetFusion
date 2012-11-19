0d0c658cb1e2863c64949526e0b1697602c6df66
Michael Gerlich <mgerlich@ipb-halle.de>
2012-11-19 17:12:38 +0000

    Added enumeration for result tabs.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@4209 88837528-0963-4635-b47b-91f4607bead6

---
+++ src/main/java/de/ipbhalle/enumerations/ResultTabs.java
@@ -0,0 +1,11 @@
+/**
+ * created by Michael Gerlich, Nov 19, 2012 - 5:26:14 PM
+ */ 
+
+package de.ipbhalle.enumerations;
+
+public enum ResultTabs {
+
+	cluster, fragmenter, database, unused;
+	
+}
