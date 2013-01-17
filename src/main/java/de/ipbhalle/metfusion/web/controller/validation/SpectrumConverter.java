08f8977d1197a780382775eb812776fadbe78807
Michael Gerlich <mgerlich@ipb-halle.de>
2013-01-17 14:13:24 +0000

    Improved spectrum conversion by adding also intensity normalization to maximum intensity.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@4332 88837528-0963-4635-b47b-91f4607bead6

--- src/main/java/de/ipbhalle/metfusion/web/controller/validation/SpectrumConverter.java
+++ src/main/java/de/ipbhalle/metfusion/web/controller/validation/SpectrumConverter.java
@@ -23,6 +23,61 @@ public class SpectrumConverter implements Converter {
 	
 	public SpectrumConverter() {	}
 	
+	private String normalizePeaks(String peaks) {
+		double maxInt = 0d;
+		String[] lines = peaks.split(linebreak);	// each m/z int pair one line
+		boolean oneLine = lines.length == 1 ? true : false;
+		StringBuilder sb = new StringBuilder();
+		if (oneLine) { // only one line detected, maybe multiple whitespace separated peaks
+			String[] temp = lines[0].trim().split(DEFAULT_WHITESPACE);
+			if (temp.length % 2 == 0) {
+				for (int i = 0; i < temp.length - 1; i += 2) { // skip each 2nd entry
+					sb.append(temp[i]).append(DEFAULT_WHITESPACE).append(temp[i + 1]).append(linebreak);
+				}
+				lines = sb.toString().trim().split(linebreak);		// overwrite lines[] if spectrum information was in a single line
+			} else {
+				System.err.println("Probably invalid spectrum format!");
+				return sb.toString().trim();
+			}
+		}
+		
+		String[] mz = new String[lines.length];
+		String[] ints = new String[lines.length];
+		for (int i = 0; i < lines.length; i++) {
+			lines[i] = lines[i].replaceAll("[ \t\\x0B\f\r]+", DEFAULT_WHITESPACE).trim();	// replace each white space by default one
+			String[] split = lines[i].split(DEFAULT_WHITESPACE);
+			if(split.length == 1) {		// only m/z, set relative intensity to 500
+				mz[i] = split[0];
+				ints[i] = "500";
+			}
+			else if(split.length == 2) {		// m/z intensity pair
+				double intensity = Double.parseDouble(split[1]);
+				if(intensity > maxInt)
+					maxInt = intensity;
+				mz[i] = split[0];
+				ints[i] = split[1];
+			}
+			else if(split.length == 3) {// m/z intensity rel.intensity triple
+				double intensity = Double.parseDouble(split[2]);
+				if(intensity > maxInt)
+					maxInt = intensity;
+				mz[i] = split[0];
+				ints[i] = split[2];
+			}
+			else {	// unknown split - assume length 3 split
+				System.err.println("Unusual size of split!");
+				mz[i] = split[0];
+				ints[i] = split[2];
+			}
+		}
+		sb = new StringBuilder();
+		//calculate the rel. intensity
+		for (int i = 0; i < lines.length; i++) {
+			sb.append(mz[i]).append(DEFAULT_WHITESPACE).append(Math.round(((Double.valueOf(ints[i]) / maxInt) * 999))).append("\n");
+		}
+		
+		return sb.toString();
+	}
 	/**
 	 * converts the given string value into an Object.
 	 */
@@ -36,35 +91,7 @@ public class SpectrumConverter implements Converter {
 		// one peak per line
 		
 		peaklist = peaklist.replaceAll("[ \t\\x0B\f\r]+", DEFAULT_WHITESPACE);
-		
-		StringBuilder sb = new StringBuilder();
-		String[] split = peaklist.split(linebreak);
-		boolean oneLine = split.length == 1 ? true : false;
-		if(oneLine) {	// only one line detected, maybe multiple whitespace separated peaks
-			String[] temp = split[0].trim().split(DEFAULT_WHITESPACE);
-			if(temp.length % 2 == 0) {
-				for (int i = 0; i < temp.length-1; i += 2) {	// skip each 2nd entry
-					sb.append(temp[i]).append(DEFAULT_WHITESPACE).append(temp[i+1]).append(linebreak);
-				}
-			}
-			else System.err.println("Probably invalid spectrum format!");
-		}
-		else {	// more than one line with peaks
-			for (int i = 0; i < split.length; i++) {
-				split[i] = split[i].trim();
-				String[] temp = split[i].split(DEFAULT_WHITESPACE);
-				if(temp.length == 1) {	// only mz information -> append default intensity
-					sb.append(temp[0]).append(DEFAULT_WHITESPACE).append(DEFAULT_INT).append(linebreak);
-				}
-				else if(temp.length == 3) {	// mz abs.int. and rel.int information -> remove absolute intensity
-					sb.append(temp[0]).append(DEFAULT_WHITESPACE).append(temp[2]).append(linebreak);
-				}
-				else sb.append(split[i]).append(linebreak);
-			}
-		}
-			
-		String s = sb.toString().trim();
-		return s;
+		return normalizePeaks(peaklist);
 	}
 
 	/**
