2118c413d99b01f4310ac96d796979f7b803620d
Michael Gerlich <mgerlich@ipb-halle.de>
2013-01-17 09:04:02 +0000

    Added different setups for different datasets.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@4331 88837528-0963-4635-b47b-91f4607bead6

--- src/test/java/de/ipbhalle/GridEngine/main/GenerateArrayJob.java
+++ src/test/java/de/ipbhalle/GridEngine/main/GenerateArrayJob.java
@@ -18,33 +18,39 @@ public class GenerateArrayJob {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		//File spectraDir = new File("/home/mgerlich/Datasets/allSpectra/");
+		File spectraDir = new File("/home/mgerlich/Datasets/allSpectra/");
 		//File spectraDir = new File("/home/mgerlich/Datasets/Eawag_IPB_TP_all/recdata/");
 		//File spectraDir = new File("/home/mgerlich/Datasets/Uni_Jena/recdata/");
 		//File spectraDir = new File("/home/mgerlich/Datasets/Pesticides_new/Pesticides_111028/recdata/");
-		//File spectraDir = new File("/vol/data_extern/ryona@psc.riken.jp/queries20121003/lipid_MS2/120918_007P_pos_ammonium_adduct/");
+		//File spectraDir = new File("/vol/data_extern/ryona@psc.riken.jp/queries20121003/lipid_MS2/120918_008P_pos_ammonium_adduct/");
 		//File spectraDir = new File("/home/mgerlich/projects/jan_stanstrup/rungroup6/");
-		File spectraDir = new File("/vol/data_extern/ryona@psc.riken.jp/queries20121003/secmet_QTOF/leaf_n__secmet_QTOF/");
-		File[] files = spectraDir.listFiles(new FileNameFilterImpl("", "txt"));
+		//File spectraDir = new File("/vol/data_extern/ryona@psc.riken.jp/queries20121003/secmet_QTOF/root_n_secmet_QTOF/");
+		//File spectraDir = new File("/home/mgerlich/projects/MTBLSFarag/profile/mf/");
+		File[] files = spectraDir.listFiles(new FileNameFilterImpl("", "txt"));	// TODO txt oder mf entsprechend switch -mf oder -record!
 		Arrays.sort(files);
 		
 		// the output directory of the grid engine runs
+		String outputDir = "/home/mgerlich/projects/metfusion_benchmark/";
 		//String outputDir = "/home/mgerlich/projects/metfusion_CE_spectra/";
 		//String outputDir = "/home/mgerlich/projects/eval_metfusion_ECFP/exactMass_noFilter/";
 		//String outputDir = "/home/mgerlich/projects/metfusion_pesticide_spectra/";
 		//String outputDir = "/home/mgerlich/projects/eval_metfusion_uniqueFilter_14092012/";
 		//String outputDir = "/home/mgerlich/projects/metfusion_tp/results_18-10-2012";
-		//String outputDir = "/vol/data_extern/ryona@psc.riken.jp/queries20121003/results/120918_007P_pos_ammonium_adduct/";
+		//String outputDir = "/vol/data_extern/ryona@psc.riken.jp/queries20121003/results/120918_008P_pos_ammonium_adduct/";
 		//String outputDir = "/home/mgerlich/projects/jan_stanstrup/results/rungroup6/";
-		String outputDir = "/vol/data_extern/ryona@psc.riken.jp/queries20121003/secmet_QTOF/leaf_n__secmet_QTOF/results/";
-
+		//String outputDir = "/vol/data_extern/ryona@psc.riken.jp/queries20121003/secmet_QTOF/root_n_secmet_QTOF/results_chebi/";
+		//String outputDir = "/home/mgerlich/projects/metfusion_tp/results_dbchemspider_Massbank_wo_Eawag_but_wUFZ_formula/";
+		//String outputDir = "/home/mgerlich/projects/metfusion_chebi/";
+		//String outputDir = "/home/mgerlich/projects/metfusion_tp/results_dbchemspider_Massbank_w_Eawag/";
+		//String outputDir = "/home/mgerlich/projects/farag_profile/results_03-01-2013/";
+		
 		// the directory in which the jar file is located
 		String projectDir = "/home/mgerlich/projects/";
 
 		// the directory where the shell scripts are stored
 		//String workDir = "/home/mgerlich/projects/eval_metfusion_ECFP/";
 		String workDir = projectDir;
-		String prefix = "leaf_n";
+		String prefix = "benchmark";
 		File jobInfo = new File(workDir, prefix + "_sge_metfusion.sh");
 		jobInfo.createNewFile();
 		jobInfo.setExecutable(true);
@@ -56,7 +62,11 @@ public class GenerateArrayJob {
 		File paramFile = new File(workDir, prefix + "_sge_metfusion.params");
 		generateParametersFile(paramFile, files, params);
 		
+		/**
+		 * TODO: change jar name
+		 */
 		File jarName = new File(projectDir, "metfusion_batch_latest.jar");
+		//File jarName = new File(projectDir, "metfusion_batch_latest.jar");
 		File scriptName = new File(workDir, prefix + "_sge_metfusion.q");
 		generateShellScript(scriptName, paramFile.getAbsolutePath(), jarName.getAbsolutePath());
 		
@@ -70,8 +80,16 @@ public class GenerateArrayJob {
 		// adjust according to number of parameters
 		fw.write("awk \"NR==$SGE_TASK_ID\" " + paramFile + " | while read A B; do echo $A/$B\n");
 		//fw.write("java -jar -Dproperty.file.path=/home/mgerlich/workspace_new/MetFusion/WebContent/WEB-INF/ " + jarName + " -mf $A -out $B -unique\n");
-		fw.write("java -jar -Xmx4000m -Dproperty.file.path=/home/mgerlich/workspace_new/MetFusion/WebContent/WEB-INF/ " + jarName 
-				+ " -mf $A -out $B -unique\n");
+		
+//		fw.write("java -jar -Dproperty.file.path=/home/mgerlich/workspace_new/MetFusion/WebContent/WEB-INF/ " + jarName + 
+//				" -mf $A -out $B -unique -format SDF -db chebi\n");
+		
+		fw.write("java -jar -Dproperty.file.path=/home/mgerlich/workspace_new/MetFusion/WebContent/WEB-INF/ " + jarName + 
+		" -record $A -out $B -unique -format SDF \n");
+		
+//		fw.write("java -jar -Xmx4000m -Dproperty.file.path=/home/mgerlich/workspace_new/MetFusion/WebContent/WEB-INF/ " + jarName 
+//				+ " -record $A -out $B -unique -format SDF -server http://msbi.ipb-halle.de/MassBank/\n");
+		
 		//fw.write("EOF\n");
 		fw.write("done");
 		
@@ -96,7 +114,20 @@ public class GenerateArrayJob {
 		fw.write("#!/bin/bash\n\n");	// shell header
 		
 		//fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/ryona/ -o /home/mgerlich/SGE/output/ryona/ -q " + queue + " " + qFile + "\n");
-		fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/secmet/ -o /home/mgerlich/SGE/output/secmet/ -q " + queue + " " + qFile + "\n");
+		//fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/secmet/ -o /home/mgerlich/SGE/output/secmet/ -q " + queue + " " + qFile + "\n");
+		//fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/lipid/ -o /home/mgerlich/SGE/output/lipid/ -q " + queue + " -pe orte2 6 " + qFile + "\n");
+		
+//		fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/farag/profile/ -o /home/mgerlich/SGE/output/farag/profile/ -q " + queue + 
+//				" -pe orte2 6 -tc 20 " + qFile + "\n");
+		
+		fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/benchmark/ -o /home/mgerlich/SGE/output/benchmark/ -q " + queue + 
+				" -pe orte2 6 -tc 20 " + qFile + "\n");
+		
+//		fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/tp/results_dbchemspider_Massbank_wo_Eawag_but_wUFZ_formula/" +
+//				" -o /home/mgerlich/SGE/output/tp/results_dbchemspider_Massbank_wo_Eawag_but_wUFZ_formula/ -q " + queue + " -pe orte2 6 " + qFile + "\n");
+		
+//		fw.write("qsub -t " + start + "-" + end + " -e /home/mgerlich/SGE/error/tp/results_dbchemspider_Massbank_w_Eawag/" +
+//				" -o /home/mgerlich/SGE/output/tp/results_dbchemspider_Massbank_w_Eawag/ -q " + queue + " -pe orte2 6 " + qFile + "\n");
 		
 		fw.flush();
 		fw.close();
