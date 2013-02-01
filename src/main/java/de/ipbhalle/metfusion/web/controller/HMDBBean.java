7e03324a446d8e68813bce0fd5e9cfde6f0baa0f
Michael Gerlich <mgerlich@ipb-halle.de>
2013-02-01 17:25:42 +0000

    Added example queries to HMDB search.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@4390 88837528-0963-4635-b47b-91f4607bead6

--- src/main/java/de/ipbhalle/metfusion/web/controller/HMDBBean.java
+++ src/main/java/de/ipbhalle/metfusion/web/controller/HMDBBean.java
@@ -31,6 +31,59 @@ public class HMDBBean implements GenericDatabaseBean {
 
 	}
 
+	public static void main(String[] args) {
+		String MS = "http://www.hmdb.ca/spectra/ms/search?" +
+				"utf8=%E2%9C%93" +
+				"&query_masses=175%0D%0A209.98%0D%0A2011.971" +
+				"&tolerance=0.05" +
+				"&mode=positive" +		// negative or neutral
+				"&commit=Search";
+		
+		String MSMS = "http://www.hmdb.ca/spectra/ms_ms/search?" +
+				"utf8=%E2%9C%93" +
+				"&parent_ion_mass=146.0" +
+				"&parent_ion_mass_tolerance=0.1" +
+				"&ionization_mode=Positive" +	// Negative or "" if NA
+				"&collision_energy_level=Low" +		// Medium or High or "" if All
+				"&peaks=40.948+0.174%0D%0A56.022+0.424%0D%0A84.37+53.488%0D%0A101.50+8.285%0D%0A102.401+0.775%0D%0A129.670+100.000%0D%0A146.966+20.070" +
+				"&mass_charge_tolerance=0.5" +
+				"&commit=Search";
+		
+		String GCMS = "http://www.hmdb.ca/spectra/c_ms/search?" +
+				"utf8=%E2%9C%93" +
+				"&retention_type=retention_index" +		// oder retention_time
+				"&retention=1072" +
+				"&retention_tolerance=1" +
+				"&peaks=73%0D%0A147%0D%0A117%0D%0A190%0D%0A191%0D%0A148%0D%0A66%0D%0A75" +
+				"&mass_charge_tolerence=" +
+				"&commit=Search";
+		
+		String NMR1D = "http://www.hmdb.ca/spectra/nmr_one_d/search?" +
+				"utf8=%E2%9C%93" +
+				"&nucleus=1H" +		// 13C
+				"&peaks=3.81%0D%0A3.82%0D%0A3.83%0D%0A3.85%0D%0A3.89%0D%0A3.90%0D%0A3.91%0D%0A4.25%0D%0A4.26%0D%0A4.27%0D%0A4.41%0D%0A8.19%0D%0A8.31" +
+				"&cs_tolerance=0.02" +
+				"&commit=Search";
+		
+		String NMR2D = "http://www.hmdb.ca/spectra/nmr_two_d/search?" +
+				"utf8=%E2%9C%93" +
+				"&library=tocsy" +	// hsqc
+				"&peaks=3.76+2.126%0D%0A3.76+2.446%0D%0A3.76+3.76%0D%0A2.446+2.126%0D%0A2.446+2.446%0D%0A2.446+3.76%0D%0A2.126+2.126%0D%0A2.126+2.446%0D%0A2.126+3.76" +
+				"&x_tolerance=0.02" +
+				"&y_tolerance=0.02" +
+				"&commit=Search";
+		
+		String utf8 = "✓";	// %E2%9C%93
+		System.out.printf("http://www.hmdb.ca/spectra/nmr_two_d/search?" +
+				"utf8=" + utf8 + 
+				"&library=%s" +	// hsqc
+				"&peaks=%s" +
+				"&x_tolerance=%f" +
+				"&y_tolerance=$f" +
+				"&commit=Search", "tocsy", "3.76+2.126%0D%0A3.76+2.446%0D%0A3.76+3.76%0D%0A2.446+2.126%0D%0A2.446+2.446%0D%0A2.446+3.76%0D%0A2.126+2.126%0D%0A2.126+2.446%0D%0A2.126+3.76",
+								0.02, 0.02);
+	}
+	
 	@Override
 	public String getDatabaseName() {
 		return databaseName;
