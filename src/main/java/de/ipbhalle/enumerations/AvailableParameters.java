7f4cc0340c92035618d279156dfdf300a5d3d172
Michael Gerlich <mgerlich@ipb-halle.de>
2013-02-01 15:17:36 +0000

    Added sdfFile to available parameters.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@4383 88837528-0963-4635-b47b-91f4607bead6

--- src/main/java/de/ipbhalle/enumerations/AvailableParameters.java
+++ src/main/java/de/ipbhalle/enumerations/AvailableParameters.java
@@ -8,7 +8,7 @@ package de.ipbhalle.enumerations;
 public enum AvailableParameters {
 	mbLimit, mbCutoff, mbIonization, mbInstruments,		// MassBank parameters
 	mfDatabase, mfDatabaseIDs, mfFormula, mfLimit, mfParentIon, mfAdduct, mfExactMass, mfSearchPPM, mfMZabs, mfMZppm,	// MetFrag parameters 
-	clustering, peaks, onlyCHNOPS,					// MetFusion parameters
+	clustering, peaks, onlyCHNOPS, sdfFile,				// MetFusion parameters
 	substrucPresent, substrucAbsent,		// substructure search includes/excludes
 	spectralDB;								// name of spectral DB, MassBank, Metlin, GMD, SDF
 }
