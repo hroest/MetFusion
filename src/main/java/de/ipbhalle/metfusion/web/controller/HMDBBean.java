cc894831ca4d523136cc4084fa38fe6c31c9228b
Michael Gerlich <mgerlich@ipb-halle.de>
2013-02-07 11:22:01 +0000

    Updated query behavior. Added default values for query parameters.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@4408 88837528-0963-4635-b47b-91f4607bead6

--- src/main/java/de/ipbhalle/metfusion/web/controller/HMDBBean.java
+++ src/main/java/de/ipbhalle/metfusion/web/controller/HMDBBean.java
@@ -5,6 +5,8 @@
 package de.ipbhalle.metfusion.web.controller;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +22,9 @@ import de.ipbhalle.metfusion.wrapper.Result;
 public class HMDBBean implements GenericDatabaseBean {
 
 	private String databaseName = "HMDB";
+	private String structurePath = "/home/mgerlich/Downloads/HMDB/structures/";
+	private final String urlHMDB = "http://www.hmdb.ca/";
+	
 	private String sessionPath;
 	private String serverUrl;
 	private List<Result> results;
@@ -43,9 +48,9 @@ public class HMDBBean implements GenericDatabaseBean {
 			"utf8=" + utf8 + "&nucleus=%s" + "&peaks=%s" +
 			"&cs_tolerance=%f" + "&commit=Search";
 	private final String tableNMR1D = "nmr_one_d_search_results";
-	private String libraryNMR1D;
-	private String peaksNMR1D;
-	private float toleranceNMR1D;
+	private String libraryNMR1D = prefixH + nucleus.H.toString();
+	private String peaksNMR1D = "3.81\n3.82\n3.83\n3.85\n3.89\n3.90\n3.91\n4.25\n4.26\n4.27\n4.41\n8.19\n8.31";
+	private float toleranceNMR1D = 0.02f;
 	
 	/** 2D NMR types */
 	public enum library {tocsy, hsqc};
@@ -53,9 +58,10 @@ public class HMDBBean implements GenericDatabaseBean {
 			"utf8=" + utf8 + "&library=%s" + "&peaks=%s" +
 			"&x_tolerance=%f" + "&y_tolerance=%f" +	"&commit=Search";
 	private final String tableNMR2D = "nmr_two_d_search_results";
-	private String peaksNMR2D;
-	private float toleranceNMR2Dx;
-	private float toleranceNMR2Dy;
+	private String libraryNMR2D = library.tocsy.toString();
+	private String peaksNMR2D = "3.76 2.126\n3.76 2.446\n3.76 3.76\n2.446 2.126\n2.446 2.446\n2.446 3.76\n2.126 2.126\n2.126 2.446\n2.126 3.76";
+	private float toleranceNMR2Dx = 0.02f;
+	private float toleranceNMR2Dy = 0.02f;
 	
 	/** MS search ionization modes */
 	public enum msIon {positive, negative, neutral};
@@ -107,32 +113,14 @@ public class HMDBBean implements GenericDatabaseBean {
 		System.out.println("#results -> " + results.size());
 	}
 
-	private List<Result> performQuery(searchType type) {
+	public List<Result> performQuery(searchType type) {
 		List<Result> results = new ArrayList<Result>();
 		String query = formatURL(type);
 		
 		results = parseGCandNMRResults(query, type);
 		
-//		switch(type) {
-//		case MS:
-//			results = parseGCandNMRResults(query, type);
-//			break;
-//			
-//		case MSMS: results = parseGCandNMRResults(query, type);
-//			break;
-//			
-//		case GCMS: results = parseGCandNMRResults(query, type);
-//			break;
-//			
-//		case NMR1D: results = parseGCandNMRResults(query, type);
-//			break;
-//			
-//		case NMR2D: results = parseGCandNMRResults(query, type);
-//			break;
-//			
-//		default: System.err.println("No defined search type!");		
-//		}
-			
+		setResults(results);
+		
 		return results;
 	}
 	
@@ -141,38 +129,48 @@ public class HMDBBean implements GenericDatabaseBean {
 		
 		switch(type) {
 		case MS:
-			formatted = String.format(queryUrlMS, "Enter one mass per line", 0.1d, msIon.positive);
+			formatted = String.format(queryUrlMS, encodeValue(peaksMS), toleranceMS, modeMS);
 			break;
 			
 		case MSMS:
-			formatted = String.format(queryUrlMSMS, 272.0d, 0.1d, msmsIon.Positive, msmsCE.Low, "M/Z RI; One peak per line", 0.5d);
+			formatted = String.format(queryUrlMSMS, precursorMSMS, toleranceMSMSprecursor, modeMSMS, ceMSMS, encodeValue(peaksMSMS), toleranceMSMSmz);
 			break;
 
 		case GCMS:
-			formatted = String.format(queryUrlGCMS, gcmsType.retention_index, 1072d, 1d, "one peak per line", 1d);
+			formatted = String.format(queryUrlGCMS, typeGCMS, retentionGCMS, toleranceGCMSretention, encodeValue(peaksGCMS), toleranceGCMSmz);
 			break;
 
 		case NMR1D:
-			String lib = "";
 			if("C".equals(nucleus.C))
-				lib = prefixC + "C";
+				libraryNMR1D = prefixC + "C";
 			else if("H".equals(nucleus.H)) {
-				lib = prefixH + "H";
+				libraryNMR1D = prefixH + "H";
 			}
-			else lib = prefixH + "H";	// default to H
+			else libraryNMR1D = prefixH + "H";	// default to H
 			
-			formatted = String.format(queryUrlNMR1D, lib, "One peak per line", 0.02d);
+			formatted = String.format(queryUrlNMR1D, libraryNMR1D, encodeValue(peaksNMR1D), toleranceNMR1D);
 			break;
 
 		case NMR2D:
-			formatted = String.format(queryUrlNMR2D, library.tocsy, "One Co-ordinate per line with the numbers seperated by a space",
-					0.02d, 0.02d);
+			formatted = String.format(queryUrlNMR2D, libraryNMR2D, encodeValue(peaksNMR2D), toleranceNMR2Dx, toleranceNMR2Dy);
 			break;
 
 		default: System.err.println("No defined search type!");		
 		}
 		
+		
 		return formatted;
+	}
+	
+	private String encodeValue(String value) {
+		String encoded = "";
+		try {
+			encoded = URLEncoder.encode(value, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			System.err.println("Unsupported encoding found for URLEncoder!");
+			encoded = value;
+		}
+		return encoded;
 	}
 	
 	private List<Result> parseGCandNMRResults(String url, searchType type) {
@@ -194,9 +192,13 @@ public class HMDBBean implements GenericDatabaseBean {
 		// MSMS has 7 columns
 		// MS has 6 columns but multiple tables
 		Element table = null;
+		Elements tablesMS = null;
 		switch(type) {
 		
-		case MS: table = doc.getElementById(tableMS); break;
+		case MS: 
+			table = doc.getElementsByClass(tableMS).first(); 
+			tablesMS = doc.getElementsByClass(tableMS); 
+			break;
 		case MSMS: table = doc.getElementById(tableMSMS); break;
 		case NMR1D: table = doc.getElementById(tableNMR1D); break;
 		case NMR2D: table = doc.getElementById(tableNMR2D); break;
@@ -207,33 +209,18 @@ public class HMDBBean implements GenericDatabaseBean {
 		if(table == null)
 			return results;
 		
+		// if MS search mit multiple entries
+		if(type.equals(searchType.MS)) {
+			for (Element element : tablesMS) {
+				results = queryMSTables(element, results);
+			}
+			return results;
+		}
+		
 		Elements rows = table.getElementsByTag("tr");
-		String rowDivider = "<hr />";
-		String thStart = "<th>";
-		String thEnd = "</th>";
-		String structurePath = "/home/mgerlich/Downloads/HMDB/structures/";
-		String urlHMDB = "http://www.hmdb.ca/";
 		MassBankUtilities mbu = new MassBankUtilities(structurePath);
 		
 		for (Element element : rows) {
-			Elements header = element.getElementsByTag("th");
-			if(!header.isEmpty()) {
-				for (Element head : header) {
-					String text = head.toString();
-					if(text.contains(rowDivider)); {
-						String[] split = text.split(rowDivider);
-						String s = "";
-						for (int i = 0; i < split.length; i++) {
-							s = split[i].trim();
-							if(s.startsWith(thStart))
-								s= s.replace(thStart, "");
-							if(s.endsWith(thEnd))
-								s = s.replace(thEnd, "");
-						}
-					}
-				}
-			}
-			
 			Elements data = element.getElementsByTag("td");
 			int columnCounter = 0;
 			String id = "";				// HMDB ID
@@ -257,7 +244,7 @@ public class HMDBBean implements GenericDatabaseBean {
 						String[] split = d.text().trim().split(" ");
 						if(split.length > 2) {	// name has two or more entries
 							for (int i = 0; i < split.length - 1; i++) {
-								name += split[i];
+								name += split[i] + " ";
 							}
 							cas = split[split.length-1];
 						}
@@ -265,6 +252,8 @@ public class HMDBBean implements GenericDatabaseBean {
 							name = split[0];
 							cas = split[1];
 						}
+						
+						name = name.trim();
 					}
 					
 					// formula + weight
@@ -296,7 +285,64 @@ public class HMDBBean implements GenericDatabaseBean {
 				
 				IAtomContainer ac = mbu.getContainer(id, structurePath);
 				
-				Result r = new Result("HMDB", id, name, score, ac, urlHMDB + link, "image", formula, weight);
+				Result r = new Result(databaseName, id, name, score, ac, urlHMDB + link, "image", formula, weight);
+				results.add(r);
+			}
+		}
+		
+		return results;
+	}
+	
+	private List<Result> queryMSTables(Element table, List<Result> current) {
+		List<Result> results = current;
+		
+		Elements rows = table.getElementsByTag("tr");
+		String structurePath = "/home/mgerlich/Downloads/HMDB/structures/";
+		String urlHMDB = "http://www.hmdb.ca/";
+		MassBankUtilities mbu = new MassBankUtilities(structurePath);
+		
+		for (Element element : rows) {
+			Elements data = element.getElementsByTag("td");
+			int columnCounter = 0;
+			String id = "";				// HMDB ID
+			String name = "";			// name
+			String adduct = "";			// adduct
+			double adductWeith = 0.0d;	// adduct MW
+			String formula = "";		// molecular formula
+			double weight = 0.0d;		// molecular weight
+			double delta = 0.0d;		// Delta = abs( query mass - adduct mass )
+			String link = "";
+			if(!data.isEmpty()) {
+				for (Element d : data) {
+					String s = d.toString();
+					// link + ID
+					if(columnCounter == 0 && s.contains("<a")) {
+						id = d.text();
+						link = d.getElementsByTag("a").get(0).attr("href");
+					}
+					else if(columnCounter == 1) {	// name
+						name = d.text().trim();
+					}
+					else if(columnCounter == 2 ) {	// adduct
+						adduct = d.text().trim();
+						formula = adduct;
+					}
+					else if(columnCounter == 3) {	// adduct MW
+						adductWeith = Double.parseDouble(d.text().trim());
+					}
+					else if(columnCounter == 4) {	// compound MW
+						weight = Double.parseDouble(d.text().trim());
+					}
+					else if(columnCounter == 5) {	// delta
+						delta = Double.parseDouble(d.text().trim());
+					}
+					
+					columnCounter++;
+				}
+				
+				IAtomContainer ac = mbu.getContainer(id, structurePath);
+				
+				Result r = new Result(databaseName, id, name, delta, ac, urlHMDB + link, "image", formula, weight);
 				results.add(r);
 			}
 		}
@@ -305,158 +351,9 @@ public class HMDBBean implements GenericDatabaseBean {
 	}
 	
 	public static void main(String[] args) throws IOException {
-		String MS = "http://www.hmdb.ca/spectra/ms/search?" +
-				"utf8=%E2%9C%93" +
-				"&query_masses=175%0D%0A209.98%0D%0A2011.971" +
-				"&tolerance=0.05" +
-				"&mode=positive" +		// negative or neutral
-				"&commit=Search";
 		
-		String MSMS = "http://www.hmdb.ca/spectra/ms_ms/search?" +
-				"utf8=%E2%9C%93" +
-				"&parent_ion_mass=146.0" +
-				"&parent_ion_mass_tolerance=0.1" +
-				"&ionization_mode=Positive" +	// Negative or "" if NA
-				"&collision_energy_level=Low" +		// Medium or High or "" if All
-				"&peaks=40.948+0.174%0D%0A56.022+0.424%0D%0A84.37+53.488%0D%0A101.50+8.285%0D%0A102.401+0.775%0D%0A129.670+100.000%0D%0A146.966+20.070" +
-				"&mass_charge_tolerance=0.5" +
-				"&commit=Search";
-		
-		String GCMS = "http://www.hmdb.ca/spectra/c_ms/search?" +
-				"utf8=%E2%9C%93" +
-				"&retention_type=retention_index" +		// oder retention_time
-				"&retention=1072" +
-				"&retention_tolerance=1" +
-				"&peaks=73%0D%0A147%0D%0A117%0D%0A190%0D%0A191%0D%0A148%0D%0A66%0D%0A75" +
-				"&mass_charge_tolerence=" +
-				"&commit=Search";
-		
-		String NMR1D = "http://www.hmdb.ca/spectra/nmr_one_d/search?" +
-				"utf8=%E2%9C%93" +
-				"&nucleus=1H" +		// 13C
-				"&peaks=3.81%0D%0A3.82%0D%0A3.83%0D%0A3.85%0D%0A3.89%0D%0A3.90%0D%0A3.91%0D%0A4.25%0D%0A4.26%0D%0A4.27%0D%0A4.41%0D%0A8.19%0D%0A8.31" +
-				"&cs_tolerance=0.02" +
-				"&commit=Search";
-		
-		String NMR2D = "http://www.hmdb.ca/spectra/nmr_two_d/search?" +
-				"utf8=%E2%9C%93" +
-				"&library=tocsy" +	// hsqc
-				"&peaks=3.76+2.126%0D%0A3.76+2.446%0D%0A3.76+3.76%0D%0A2.446+2.126%0D%0A2.446+2.446%0D%0A2.446+3.76%0D%0A2.126+2.126%0D%0A2.126+2.446%0D%0A2.126+3.76" +
-				"&x_tolerance=0.02" +
-				"&y_tolerance=0.02" +
-				"&commit=Search";
-		
-		String utf8 = "✓";	// %E2%9C%93
-		System.out.printf("http://www.hmdb.ca/spectra/nmr_two_d/search?" +
-				"utf8=" + utf8 + 
-				"&library=%s" +	// hsqc
-				"&peaks=%s" +
-				"&x_tolerance=%f" +
-				"&y_tolerance=$f" +
-				"&commit=Search", "tocsy", "3.76+2.126%0D%0A3.76+2.446%0D%0A3.76+3.76%0D%0A2.446+2.126%0D%0A2.446+2.446%0D%0A2.446+3.76%0D%0A2.126+2.126%0D%0A2.126+2.446%0D%0A2.126+3.76",
-								0.02, 0.02);
-		
-		// parse HTML with jsoup
-		Document doc = Jsoup.connect(GCMS).get();	//Jsoup.parse(s);
-		
-		// table rows
-		//Elements rows = doc.getElementsByTag("table");	// first row contains header <th>, afterwards only data <td>
-		Element table = doc.getElementById("nmr_two_d_search_results");	// first row contains header <th>, afterwards only data <td>
-		Elements rows = table.getElementsByTag("tr");
-		String rowDivider = "<hr />";
-		String thStart = "<th>";
-		String thEnd = "</th>";
-		String structurePath = "/home/mgerlich/Downloads/HMDB/structures/";
-		String urlHMDB = "http://www.hmdb.ca/";
-		MassBankUtilities mbu = new MassBankUtilities(structurePath);
-		List<Result> results = new ArrayList<Result>();
-		
-		for (Element element : rows) {
-			Elements header = element.getElementsByTag("th");
-			if(!header.isEmpty()) {
-				for (Element head : header) {
-					String text = head.toString();
-					if(text.contains(rowDivider)); {
-						String[] split = text.split(rowDivider);
-						String s = "";
-						for (int i = 0; i < split.length; i++) {
-							s = split[i].trim();
-							if(s.startsWith(thStart))
-								s= s.replace(thStart, "");
-							if(s.endsWith(thEnd))
-								s = s.replace(thEnd, "");
-						}
-					}
-				}
-			}
-			
-			Elements data = element.getElementsByTag("td");
-			int columnCounter = 0;
-			String id = "";				// HMDB ID
-			String name = "";			// name
-			String cas = "";			// CAS ID
-			String formula = "";		// molecular formula
-			double weight = 0.0d;		// molecular weight
-			double score = 0.0d;		// library matches
-			String link = "";
-			if(!data.isEmpty()) {
-				for (Element d : data) {
-					String s = d.toString();
-					// link + ID
-					if(columnCounter == 0 && s.contains("<a")) {
-						id = d.text();
-						link = d.getElementsByTag("a").get(0).attr("href");
-					}
-					
-					// name + CAS
-					if(columnCounter == 1 && s.contains("<strong>")) {
-						String[] split = d.text().trim().split(" ");
-						if(split.length > 2) {	// name has two or more entries
-							for (int i = 0; i < split.length - 1; i++) {
-								name += split[i];
-							}
-							cas = split[split.length-1];
-						}
-						else if(split.length == 2) {
-							name = split[0];
-							cas = split[1];
-						}
-					}
-					
-					// formula + weight
-					if(columnCounter == 2 && s.contains("<sub>")) {
-						String[] split = d.text().trim().split(" ");
-						if(split.length == 2) {
-							weight = Double.parseDouble(split[0]);
-							formula = split[1];
-						}
-					}
-					
-					// image - > columnCounter == 3
-					
-					// score in the form of x/y
-					if(columnCounter == 4) {
-						String s2 = d.text().trim();
-						if(s2.contains("/")) {
-							String[] split = s2.split("/");
-							if(split.length == 2) {
-								double d1 = Double.parseDouble(split[0]);
-								double d2 = Double.parseDouble(split[1]);
-								score = d1 / d2;
-							}
-						}
-						else score = Double.parseDouble(d.text().trim());
-					}
-					columnCounter++;
-				}
-				
-				IAtomContainer ac = mbu.getContainer(id, structurePath);
-				
-				Result r = new Result("HMDB", id, name, score, ac, urlHMDB + link, "image", formula, weight);
-				results.add(r);
-			}
-		}
-		
+		HMDBBean hb = new HMDBBean();
+		List<Result> results = hb.performQuery(HMDBBean.searchType.MS);
 		System.out.println("Found [" + results.size() + "] results");
 		System.out.println("ID\tName\tFormula\tWeight\tScore");
 		for (Result result : results) {
@@ -581,6 +478,14 @@ public class HMDBBean implements GenericDatabaseBean {
 		this.toleranceNMR1D = toleranceNMR1D;
 	}
 
+	public String getLibraryNMR2D() {
+		return libraryNMR2D;
+	}
+
+	public void setLibraryNMR2D(String libraryNMR2D) {
+		this.libraryNMR2D = libraryNMR2D;
+	}
+	
 	public String getPeaksNMR2D() {
 		return peaksNMR2D;
 	}
@@ -723,6 +628,14 @@ public class HMDBBean implements GenericDatabaseBean {
 
 	public void setToleranceGCMSmz(float toleranceGCMSmz) {
 		this.toleranceGCMSmz = toleranceGCMSmz;
+	}
+
+	public String getStructurePath() {
+		return structurePath;
+	}
+
+	public void setStructurePath(String structurePath) {
+		this.structurePath = structurePath;
 	}
 
 }
