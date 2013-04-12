7214c18b661603af7f5efbcc32155ce8b7b89362
Michael Gerlich <mgerlich@ipb-halle.de>
2013-04-12 12:28:58 +0000

    Added working method for reading in SD files.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@4608 88837528-0963-4635-b47b-91f4607bead6

--- src/main/java/de/ipbhalle/metfusion/main/SDFDatabase.java
+++ src/main/java/de/ipbhalle/metfusion/main/SDFDatabase.java
@@ -4,11 +4,28 @@
 
 package de.ipbhalle.metfusion.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
-import de.ipbhalle.metfrag.main.MetFrag;
+import org.openscience.cdk.ChemFile;
+import org.openscience.cdk.ChemObject;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IMolecularFormula;
+import org.openscience.cdk.io.MDLV2000Reader;
+import org.openscience.cdk.tools.CDKHydrogenAdder;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
+import org.openscience.cdk.tools.manipulator.ChemFileManipulator;
+import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
+
 import de.ipbhalle.metfusion.web.controller.GenericDatabaseBean;
 import de.ipbhalle.metfusion.wrapper.Result;
+import de.ipbhalle.metfusion.wrapper.ResultComparator;
 
 public class SDFDatabase implements GenericDatabaseBean {
 
@@ -23,16 +40,130 @@ public class SDFDatabase implements GenericDatabaseBean {
 	private boolean uniqueInchi;
 	private int searchProgress;
 	
-	public SDFDatabase() {
-		// TODO Auto-generated constructor stub
+	private String SDFfile;
+	
+	
+	public SDFDatabase(String databaseName, String sdfFilePath) {
+		this.databaseName = databaseName;
+		this.setSDFfile(sdfFilePath);
+		
+		this.results = new ArrayList<Result>();
+		this.unused = new ArrayList<Result>();
 	}
 	
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		//MetFrag.startConvenienceSDF(null, done, searchProgress, searchProgress, searchProgress, done, done, searchProgress, done, done, done, done, searchProgress, done, databaseName);
+		
+		// read in SDF file thread-wise
+		MDLV2000Reader reader = null;
+		List<IAtomContainer> containersList;
+		List<IAtomContainer> ret = new ArrayList<IAtomContainer>();
+		
+		File f = new File(SDFfile);
+		
+		if(f.isFile())
+		{
+			try {
+				reader = new MDLV2000Reader(new FileReader(f));
+			} catch (FileNotFoundException e) {
+				System.err.println("SD file [" + f.getAbsolutePath() + "] not found. Returning empty list!");
+				this.results = new ArrayList<Result>();
+				
+				return;
+			}
+			ChemFile chemFile = null;
+			try {
+				chemFile = (ChemFile) reader.read((ChemObject) new ChemFile());
+				containersList = ChemFileManipulator.getAllAtomContainers(chemFile);
+				for (IAtomContainer container : containersList) {
+					Map<Object, Object> properties =  container.getProperties();
+					String id = "", name = "", origin = "", smiles = "";
+					int peaksExplained = 0;
+					double score = 0.0d;
+					
+					if(properties.containsKey("id")) {
+						id = (String) properties.get("id");
+					}
+					if(properties.containsKey("origscore")) {
+						score = Double.valueOf((String) properties.get("origscore"));
+					}
+					if(properties.containsKey("name")) {
+						name = (String) properties.get("name");
+					}
+					if(properties.containsKey("peaksExplained")) {
+						peaksExplained = Integer.valueOf((String) properties.get("peaksExplained"));
+					}
+					if(properties.containsKey("origin")) {
+						origin = (String) properties.get("origin");
+					}
+					if(properties.containsKey("smiles")) {
+						smiles = (String) properties.get("smiles");
+					}
+					
+					IAtomContainer temp = hydrogenHandling(container);
+					ret.add(temp);
+
+					// compute molecular formula
+					IMolecularFormula iformula = MolecularFormulaManipulator.getMolecularFormula(container);
+					String formula = MolecularFormulaManipulator.getHillString(iformula);
+					// compute molecular mass
+					double emass = 0.0d;
+					if (!formula.contains("R")) // compute exact mass from formula only if NO residues "R" are present
+							emass = MolecularFormulaManipulator.getTotalExactMass(iformula);
+					
+					Result r = new Result(origin, id, name, score, temp, "", "", formula, emass);
+					r.setMatchingPeaks(peaksExplained);
+					if(smiles != null && !smiles.isEmpty())
+						r.setSmiles(smiles);
+					
+					results.add(r);
+				}
+			} catch (CDKException e) {
+				System.err.println("Error reading SDF file " + f.getAbsolutePath());
+				this.results = new ArrayList<Result>();
+				
+				return;
+			}
+		}
+		else {
+			System.err.println("[" + f.getAbsolutePath() + "] is not a file! Please specify a valid SD file. Returning an empty list!");
+			this.results = new ArrayList<Result>();
+			
+			return;
+		}
+		
+		/**
+		 * apply sorting after score ???
+		 */
+		// sort final result list based upon score value
+//		Collections.sort(results, Collections.reverseOrder(new ResultComparator()));
+		/**
+		 * 
+		 */
 	}
 
+	private IAtomContainer hydrogenHandling(IAtomContainer container) {
+		// create deep copy
+		IAtomContainer copy = null;
+		try {
+			 copy = container.clone();
+		} catch (CloneNotSupportedException e1) {
+			return container;
+		}
+		
+		//add hydrogens
+        try {
+			AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(copy);
+			CDKHydrogenAdder hAdder = CDKHydrogenAdder.getInstance(copy.getBuilder());
+		    hAdder.addImplicitHydrogens(copy);
+		    AtomContainerManipulator.convertImplicitToExplicitHydrogens(copy);
+		} catch (CDKException e) {		// return original if adding H's fails
+			return container;
+		}
+        
+        return copy;
+	}
+	
 	@Override
 	public void setDatabaseName(String name) {
 		this.databaseName = name;
@@ -131,6 +262,14 @@ public class SDFDatabase implements GenericDatabaseBean {
 	@Override
 	public boolean isUniqueInchi() {
 		return uniqueInchi;
+	}
+
+	public String getSDFfile() {
+		return SDFfile;
+	}
+
+	public void setSDFfile(String sDFfile) {
+		SDFfile = sDFfile;
 	}
 
 }
