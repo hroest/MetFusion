d6790fb5173badcc60b47244e0a344b23a1d1613
Michael Gerlich <mgerlich@ipb-halle.de>
2013-12-20 16:19:03 +0000

    Draft class for querying multiple databases, merge their results and perform MetFusion.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@5551 88837528-0963-4635-b47b-91f4607bead6

---
+++ src/test/java/sandbox/CombinedRetrieval.java
@@ -0,0 +1,44 @@
+/**
+ * created by Michael Gerlich, Nov 1, 2013 - 4:15:02 PM
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */ 
+
+package sandbox;
+
+/**
+ * Utility class to retrieve structures/information from multiple databases,
+ * including InChIKey filter and providing SDF output.
+ * 
+ * @author mgerlich
+ *
+ */
+public class CombinedRetrieval {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// run threaded versions of database queries
+		
+		
+		// filter duplicate
+		
+		// write SDF
+	}
+
+}
