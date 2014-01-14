3e705ff37642e2fce33f609ca2c22720df1ad358
Michael Gerlich <mgerlich@ipb-halle.de>
2014-01-14 13:10:36 +0000

    Added draft class for CML writing via CDK.
    git-svn-id: https://msbi.ipb-halle.de/svn/msbi/src/MetFusion/trunk@5568 88837528-0963-4635-b47b-91f4607bead6

---
+++ src/test/java/sandbox/CMLTest.java
@@ -0,0 +1,57 @@
+/**
+ * created by Michael Gerlich, Jan 7, 2014 - 1:53:37 PM
+
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */ 
+
+package sandbox;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.io.CMLWriter;
+import org.openscience.cdk.smiles.SmilesParser;
+
+import de.ipbhalle.metfrag.tools.PPMTool;
+
+public class CMLTest {
+
+	/**
+	 * @param args
+	 * @throws CDKException 
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws CDKException, IOException {
+		String smiles = "O";
+		SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+		IAtomContainer ac = sp.parseSmiles(smiles);
+		
+		StringWriter sw = new StringWriter();
+//		FileWriter fw = new FileWriter("/tmp/test.cml");
+		CMLWriter cw = new CMLWriter(sw);
+		cw.write(ac);
+		cw.close();
+		
+		System.out.println(sw.toString());
+		
+		System.out.println(PPMTool.getPPMDeviation(273.014, 30));
+	}
+
+}
